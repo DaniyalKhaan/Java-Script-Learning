f28f9d3f6b1fc1f3860e9989cc7860b8410a7d16
Daniyal <daniyalkhan112001@gmail.com>
2024-02-18 04:15:31 +0500

    const variables

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,45 +1,19 @@
-// Topic: Type Conversion ( Converting one datatype to Another)
+// Topic Const (keyword)
+// const keyword make variable constant means it can not be change later in the program, you will give value only once then 
+// if try to change its value it will generate an error
 
-let age = window.prompt("Your age");
+// this is usefull in cases where we want to dont change the value of some variabel like, background theme, a color,
+// or value of pi
 
-// age = age + 1;
-console.log("You are ", age, " years old");
-// This is showing the output of 211 years old, which is not what we expected, as I already know this is 
-// because the age variable is a string, and when try to increment it it concate the characters, which is why we are seing
-// 211 instead of 22.
+// note: for const variable it is a good approach to use uppercase naming
 
-// Solution: so to solve this issue we can convert the age which is string to number, this can be done by calling
-// pre define constructor of the Number class in java script., given below is the syntax and way to do this
+// IMPLEMENTATON
 
+const COLOR = "This Color";
 
-age = Number(age);
+const PI = 3.1415926;
 
-age = age+1;
+// Now if someone try to change value to this variables, an error will be generated.
 
-console.log("You are ", age, " Years old.");
-// so know the issue is been solved and the output is "You are 22 years old."
-
-
-
-
-// Other data type conversions:
-// we can also convert other data types to required data type by calling the constructor of the targer data type
-
-let x;
-let y;
-let z;
-
-x = 83;
-y = "76";
-z = "";
-
-// we can also check that which is the data type of the variable by putting typeof keyword, implemented below;
-
-
-z = Number(z);
-y = String(y);
-z = Boolean(z);
-
-console.log(x, typeof x);
-console.log(y, typeof y);
-console.log(z, typeof z);
+console.log(COLOR);
+console.log(PI);
