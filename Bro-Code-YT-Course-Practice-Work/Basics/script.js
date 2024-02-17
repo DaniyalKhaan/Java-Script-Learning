ee9e0a6c497519dd45d9a9717af276bd652d3521
Daniyal <daniyalkhan112001@gmail.com>
2024-02-17 23:22:12 +0500

    Basics Of Java-Script

---
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -0,0 +1,22 @@
+let myName;
+let myAge;
+let isStudent;
+
+myName = "Daniyal";
+myAge = 22;
+isStudent = true;
+
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+
+console.log("My name is ", myName);
+console.log("I am " , myAge , "Years Old");
+console.log("Student ", isStudent);
+
+console.log("This is a \"double quoted\" string in a Sentence/String."); // If double quoted is needed it will be done like this
+
+// Changing values of html tags from js file
+
+document.getElementById("p1").innerHTML = " Hi how are you.";
+document.getElementById("thisH1").innerHTML = "Daniyal Muhammad Khan";
