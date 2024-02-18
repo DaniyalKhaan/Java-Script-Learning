98f152077b16db8b1413ece1c2f19649b10730da
Daniyal <daniyalkhan112001@gmail.com>
2024-02-18 19:32:59 +0500

    Counter-Program

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,33 +1,23 @@
-// Topic : Practice Program for finding a hypothenuse of a traingle
+// Making a counter Program, consist of three buttons, one for increasing, one for decreasing and final one for to reset
+// the counter.
 
-// The formula to find hyponthenuse is hypothenuse = square root of ( base square + height square)
 
-// so lets do this:
+let counter = 0;
 
-let hyponthenuse;
-let base;
-let height;
+document.getElementById("increaseButton").onclick = function(){
+    // counter = document.getElementById("counterLabel").value;
+    counter = Number(counter);
+    counter++;
+    document.getElementById("counterLabel").innerHTML = counter;
+}
 
-// height = window.prompt("Enter size of Height of the traingle in numbers");
-// // Note: Window.prompy take input as string, so make sure to convert the string to number before applying the 
-// // formula/operations
-// height = Number(height);
 
-// base = window.prompt("Enter Base Size of the traingle in numbers");
-// // Converting the string to Numeber
-// base = Number(base);
-// hyponthenuse  = Math.sqrt(Math.pow(base, 2) + Math.pow(height, 2));
-// console.log("The size of the hyponthenuse is: ", hyponthenuse);
+document.getElementById("resetButton").onclick = function(){
+    counter = 0;
+    document.getElementById("counterLabel").innerHTML = counter;
+}
 
-// Done. I have verified it is working absolutely fine, but lets make this more fine by using html textboxes for data 
-// Entry
-
-document.getElementById("summitButton").onclick = function(){
-
-    base = document.getElementById("baseTextBox").value;
-
-    height = document.getElementById("heightTextBox").value;
-
-    hyponthenuse = Math.sqrt(Math.pow(base, 2) + (Math.pow(height, 2)));
-    document.getElementById("labelForHyp").innerHTML ="Result, hypothenuse Size: " + hyponthenuse;
+document.getElementById("decreaseButton").onclick = function(){
+    counter--;
+    document.getElementById("counterLabel").innerHTML = counter;
 }
