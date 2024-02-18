177e2b399dc6fe7c7bf1cd3467df0e8d11e07086
Daniyal <daniyalkhan112001@gmail.com>
2024-02-18 23:39:18 +0500

    Simple Dices

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,23 +1,23 @@
-// Making a counter Program, consist of three buttons, one for increasing, one for decreasing and final one for to reset
-// the counter.
+// Dice game; ( Math.Random())
+// making three dices that can generate random number between 1 and 6.
 
+let firstDice;
+let secondDice;
+let thirdDice;
 
-let counter = 0;
-
-document.getElementById("increaseButton").onclick = function(){
-    // counter = document.getElementById("counterLabel").value;
-    counter = Number(counter);
-    counter++;
-    document.getElementById("counterLabel").innerHTML = counter;
-}
-
-
-document.getElementById("resetButton").onclick = function(){
-    counter = 0;
-    document.getElementById("counterLabel").innerHTML = counter;
-}
-
-document.getElementById("decreaseButton").onclick = function(){
-    counter--;
-    document.getElementById("counterLabel").innerHTML = counter;
+document.getElementById("rollButton").onclick = function(){
+    // so here is the logic of the random numbers which will be generated.
+    // I am using math random object, and to use only numbers from 1 to 6, I am *ing it by 6 also adding 1 so it does
+    // not generate 0.
+    firstDice = (Math.random() * 6) + 1;
+    // Than I am just rounding the number using math.floor() class, since math.random generates numbers in decimal als0
+    firstDice = Math.floor(firstDice);
+   
+    document.getElementById("firstDice").innerHTML = firstDice;
+    
+    secondDice = Math.floor((Math.random() * 6) +1) ;
+    document.getElementById("secondDice").innerHTML = secondDice;
+    
+    thirdDice = Math.floor((Math.random() * 6) +1) ;
+    document.getElementById("thirdDice").innerHTML = thirdDice;
 }
