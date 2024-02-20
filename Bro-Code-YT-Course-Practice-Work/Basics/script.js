8fed881dba00f2f31f64718282b3d08881b72711
Daniyal <daniyalkhan112001@gmail.com>
2024-02-20 23:17:23 +0500

    .checked property

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,15 +1,23 @@
-// Topic: If Statement in JavaScript:
+// Topic : checked property,
+// There is a propery named .checked in js by which we can find out that radio button or
+// a checkbox is selected or not selected.
 
-// There is three main if statement statements that we can use in if statement, if (Condition), else if (conditoin),
-// and else (condition)
 
-let time = 14.25;
 
-if (time < 12){
+document.getElementById("checkBoxBtnsSummit").onclick = function (){
+    if (document.getElementById("studentCheckBox").checked) {
+        console.log("Welcome Student. In which subject you want to get tuition")
+    } else if (document.getElementById("teacherCheckBox").checked){
+        console.log("Welcome Sir, How many of teaching experience do you have, and what are your experties")
+    }
+}
 
-    console.log("Good Morning!")
-} else if (time >= 12 && time <=17){ 
-     console.log("Good Afternoon!")
-} else {
-    console.log("Good Evening!")
+
+document.getElementById("radioBtnsSummit").onclick = function(){
+    const maleRadioButton = document.getElementById("maleRadioButton");
+    if (maleRadioButton.checked){
+        console.log("Male is selected: ")
+    } else if (document.getElementById("femaleRadioButton").checked){
+        console.log("Female is selected: ")
+    }
 }
