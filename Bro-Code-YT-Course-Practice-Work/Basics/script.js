d77426319fcddcd863131a4c87a419135d0ddc24
Daniyal <daniyalkhan112001@gmail.com>
2024-02-20 23:55:41 +0500

    not logical operator

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,49 +1,14 @@
-// Topic: Switch Statemant:
+// Topic: Not-logical-Operator (!).
 
-let grade = 'D';
+let marks = 60;
 
-switch(grade){
-    case('A'):
-    case(''):
-    console.log("You did great in the exam.")
-    break;
-    case('B'):
-    case('b'):
-    console.log("You did great in the exam.")
-    break;
-    case('C'):
-    case('c'):
-    console.log("You did good in the exam.")
-    break;
-    case('D'):
-    case('d'):
-    console.log("You Failed the exam.")
-    break;
-    default:
-    console.log(grade + " is not a letter grade!")
+if (!(marks>100)){
+    console.log("Pass")
+} 
 
-    
-}
+let pass = false;
 
-let percentage = 72.5;
+if (!pass){
 
-switch (true){
-    case (percentage >= 90):
-        console.log("You got grade A+");
-        break;
-    case (percentage >= 80):
-        console.log("You got grade A");
-        break;
-    case (percentage >= 70):
-        console.log("You got grade B");
-        break;
-    case (percentage >= 60):
-        console.log("You got grade C");
-        break;
-    case (percentage < 60):
-        console.log("You got grade D");
-        break;
-        
-        
-    
+    console.log("You are Fail !")
 }
