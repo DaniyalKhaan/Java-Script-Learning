662f38e1122a815806e36b54920771b1d2bad34e
Daniyal <daniyalkhan112001@gmail.com>
2024-02-21 23:28:06 +0500

    return in functions

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,23 +1,21 @@
-// Topic: Function/Methods
+// Topic: return statement property in functions
 
-// Functions in java script is the same as in java, but one thing which I find different is function parameters veriable
-// in java we decleare function varibale inside its parameters when writing a function, but this is different
-// in javascript,
+// we can return something from a function, like we can declare return statement inside a function and
+// whenever we call it from some place, it will return the value which was declared to be return to that 
+// spot(line of code) from where the function was called.
 
+let width = window.prompt("Enter the width");
+let height = window.prompt("Enter the height");
 
-iAmFuction();
-function iAmFuction (){
-    console.log("Do Something! ")
+let area = someFunction(width, height);
+// we are assigning here the value of area by calling a method, which have return statment, so that
+// the return statement is returning the value of result which is assigning to the area operator through
+// = operator.
+
+function someFunction(a, b){
+
+    let result = a * b;
+    return result;
 }
 
-
-
-
-let xValue = 349;
-let yValue = 101;
-
-functionWithParameters(xValue, yValue);
-function functionWithParameters(a, b){
-
-    console.log(a - b);
-}
+console.log("Area : " , area);
