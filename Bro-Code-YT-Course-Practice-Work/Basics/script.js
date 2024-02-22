a04ed9afbe2f6ad4349eed8411f782f5809e079b
Daniyal <daniyalkhan112001@gmail.com>
2024-02-23 04:51:20 +0500

    sorting Arrays of numbers

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,5 +1,5 @@
-// Topic : sorting arrays of String (Sorting: Alphabatically Accending or decending)
-
+// Topic : sorting arrays of String/numbers (Sorting: Alphabatically Accending or decending)
+// Note it can aslo sort numbers
 
 let someArray = ["m","o","n","e","y","is","s","o","l","u","t","i","o","n", 
 "to 85% of my problems."];
@@ -11,5 +11,16 @@ someArray.sort().reverse(); // for sorting in reverse (Z to A)
 
 
 for (let i of someArray){
+    console.log(i);
+}
+
+
+// it aslo works for numbers
+
+let number = [7,4,6,2,6,9];
+number.sort().reverse();
+number.sort();
+
+for (let i of number){
     console.log(i);
 }
