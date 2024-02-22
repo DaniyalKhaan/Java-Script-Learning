8087ee2ed1d6e2e90cc48b4677a54594e7d949b2
Daniyal <daniyalkhan112001@gmail.com>
2024-02-23 04:41:18 +0500

    sorting Arrays

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,39 +1,15 @@
-// Hint: Interested Topic (Learning something new )
-// Topic: Idderation through Arrays via for loop
-
-// if we have an array and we need to apply or do something on all of its 
-// elements we can achive this via for loop
-
-// Example:
-
-let job = ["Softwear Engineer", "Secuiry Expert","Dev-Ops Enginner", "Android Developer"];
+// Topic : sorting arrays of String (Sorting: Alphabatically Accending or decending)
 
 
-
-for ( let i = 0; i < job.length; i++){
-    console.log(job[i]);
-} // we can travel to complete array this way using for loop. Make sure 
-// that the length of the counter variable is just < (less only) than the 
-// length of the Array
+let someArray = ["m","o","n","e","y","is","s","o","l","u","t","i","o","n", 
+"to 85% of my problems."];
 
 
+someArray.sort(); // for sorting A to Z(alphabatically)
 
-// Now lets do the same in reverse order in the same array, if sometimes needed we
-// can achive Array reversily
-
-for (let i=job.length -1 ; i >= 0; i--){
-    console.log(` ${job[i]} This will print all elements in revere order`);
-}
+someArray.sort().reverse(); // for sorting in reverse (Z to A)
 
 
-// there is also another method available in java script for the same functionality
-// I was not familiar with this method, today I found this method
-// let me show you the syntax and functionality(already told you is the same)
-
-for ( let i of job){
+for (let i of someArray){
     console.log(i);
-  // isnt this very simple?
-  // keep one thing in mind that in this method we just print the counter
-  // variable it prints the complete array but in normal for loop we use 
-  // array name than inside matrix brackets [] we put counter variable name 
 }
