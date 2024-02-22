7421dfb2e9fc8244c0a68d0799b0e50bceaf9d43
Daniyal <daniyalkhan112001@gmail.com>
2024-02-23 04:23:29 +0500

    Arrays Idderation through for loop

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,28 +1,39 @@
-// Topic : Arrays
+// Hint: Interested Topic (Learning something new )
+// Topic: Idderation through Arrays via for loop
+
+// if we have an array and we need to apply or do something on all of its 
+// elements we can achive this via for loop
+
+// Example:
+
+let job = ["Softwear Engineer", "Secuiry Expert","Dev-Ops Enginner", "Android Developer"];
 
 
 
-// syntax:
-
-let fruits = ["apple", "banana", "orange", "peach"];
-
-
-// there are some usefull properties of Arrays which are given below
-
-
-// 4 +ing And -ing elements in array
-fruits.unshift("WaterMeloon");  // add new element to the start of Array
-fruits.shift();       // remove/delete first element of Array
-fruits.push("Palm Grenade"); // add an element to the last of the Array
-fruits.pop();        // remove/delete last element of the Array
-
-fruits.length;   // by this we can see the length of the array and also can assign it some variables
-// Note: .length returns the total numbers of elements present in an Array, it is not like indexes it just return the total
-// numbers of elements inside that array
-
-
-fruits.indexOf("orange");
+for ( let i = 0; i < job.length; i++){
+    console.log(job[i]);
+} // we can travel to complete array this way using for loop. Make sure 
+// that the length of the counter variable is just < (less only) than the 
+// length of the Array
 
 
 
-console.log(fruits.indexOf("orange"));
+// Now lets do the same in reverse order in the same array, if sometimes needed we
+// can achive Array reversily
+
+for (let i=job.length -1 ; i >= 0; i--){
+    console.log(` ${job[i]} This will print all elements in revere order`);
+}
+
+
+// there is also another method available in java script for the same functionality
+// I was not familiar with this method, today I found this method
+// let me show you the syntax and functionality(already told you is the same)
+
+for ( let i of job){
+    console.log(i);
+  // isnt this very simple?
+  // keep one thing in mind that in this method we just print the counter
+  // variable it prints the complete array but in normal for loop we use 
+  // array name than inside matrix brackets [] we put counter variable name 
+}
