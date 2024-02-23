6d58ade7068921360a676060c18892f4a0bdcef9
Daniyal <daniyalkhan112001@gmail.com>
2024-02-24 03:50:04 +0500

    reset Operator

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,19 +1,47 @@
-// Topic : spread operator "..." ( this operator unpacks elements)
-// spread operator (...) makes things very easy when we are dealing with Arrays and
-// String basically it can spread the complete String and Arrays complete elements
+// Topic : Rest Parameters (...)
 
-// Syntax:
+// Rest parameters also work like spread operator but instead of Arrays or 
+// Strings it works with function
 
-let iAmArray = ["a","f","h","s","w","r","b","m"];
+// Rest Parameters can make a function takes multiple parameters
+// Uses : suppose we have multiple variables and we need them for sum operation
+// in a funtion, now if there is scene where we just want two or maybe three
+// or any other number of arguments, so if we call that funtion with less arguments
+// it will generate error
 
-// console.log(iAmArray);
-// see when I am printin this array it display output as Array(8) {means an Array of 8 elements}
-// but what if we want to display all the elements, we can use spread operator 
+// SO in order to avoid this error we can use rest parameters and then 
+// we can call the funtion with parameters available it that movement, it 
+// will work with two, three , four ar any number of parameters and we
+// will not need to make multiple funtions for multiple argument senario
 
-// EXAMPLE:
 
 
-console.log(...iAmArray);
-// this time it is showing all the elements of array in output 
+// background functionality: when we use this operator it make any array for 
+// arguments and it could be one, two , or any arguments ( it accept arguments as array elements)
 
-//  we can also use ... method if we want to assign an array to another element
+
+// Syntax:  funtion funtionName(...(three dots) with any name for parameter)
+
+// EXameple: funtion with rest parameters can accept any number of arguments
+
+function functionName(...anyName){
+
+    let addition = 0;
+
+    for (let number of anyName){
+        addition += number;
+    }
+    return addition;
+}
+
+let a = 2;
+let b = 4;
+let c = 6;
+let d = 8;
+
+console.log(functionName(a, b));
+console.log(functionName(a, b, c, d));
+console.log(functionName(a, b, c, d, 100, 3, 43));
+
+// Now see this funtion will not generate any errors and we can pass any number
+// of arguments
