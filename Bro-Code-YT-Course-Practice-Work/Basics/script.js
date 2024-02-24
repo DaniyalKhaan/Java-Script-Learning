2aef67e9ff77e53836a9b2b20915ec4d856a2a34
Daniyal <daniyalkhan112001@gmail.com>
2024-02-24 23:37:51 +0500

    forEach()-3

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,16 +1,24 @@
+
+
 // Topic: forEach() method of Arrays
 
-// forEach method call a funtion for each element of an array ( basically we can iterate 
-// through array elements using this method but it add one more big functionality which is it call a function for each element )
+// forEach method call a funtion for each element of an array ( basically it iterate 
+// through array elements but also it add one more big functionality which is it call a function for each element )
 
 // the forEach() method is not executed for empty elements.
 
 let anArray = ["abcde", "ghi", "lmn", "pqr"];
 
+// This method gives as some(total 5  but 2 are required) arguments automaically which are given below( some of them are optional one is required)
 
+// Arguments : functionAsCallback(required)      element(required)    index(optional)     array(optional)   thisvalue(optional)
+// need    :         required                         required           optional              optional       optional
+
+ 
 
 
 // syntax: ArrayName.forEach(passFunctionNameHereWithoutParanthesis)
+//                              C A L L B A C K NAME
 
 
 anArray.forEach(printArray);
