895c96b0e52da1f33021b44d12c841a29d0b4a4c
Daniyal <daniyalkhan112001@gmail.com>
2024-02-24 23:19:19 +0500

    forEach()-1

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,46 +1,22 @@
-// Topic: callback in functions [ very important topic]
+// Topic: forEach() method of Arrays
 
-// callback : callback servers very crucial purpose in programming,
-// it call a function from a function when the funtion is finished(a ryme right)
-// like we can give a function name to another function in its parameters
-// and it will call this function when it is finished
+// forEach method call a funtion for each element of an array ( basically we can iterate 
+// through array elements using this method but it add one more big functionality which is it call a function for each element )
 
-// we can acheive small functionalities like calculations etc whithout callback, but
-// imagagine there is a function which reads a file here you will need callback
-// so when the file reading is complete the function called the target function afterwars
+// the forEach() method is not executed for empty elements.
 
-// it play vital role in ascynchronious operation in java script
-
-// ok, I think this points of callback are enough for now,lets goes straight to its implementation
+let anArray = ["abcde", "ghi", "lmn", "pqr"];
 
 
 
-// Example : callback funtion
 
-// Ok so I have made two funtions one for display and one is a callback funtion
-// which will take the name of the display funtion in parameters and will call
-// display(printToConsole()) but when it is finished first,
-// now lets call the callback funtion and see the result
-iAmAcallBackFunction(5, 55, printToConsole);
-// so it is working fine:
-// here are some tips about this funtion :
-// 1. when calling a callback funtion you have to give the genuone name of the 
-// target funtion without parameters as argument to the callback funtion;
+// syntax: ArrayName.forEach(passFunctionNameHereWithoutParanthesis)
 
 
-function printToConsole(takeSum){
-    console.log(takeSum);
+anArray.forEach(printArray);
+// when we pass printArray here it call the function and execute the operation
+// for every element in anArray
 
+function printArray (arrayElements){
+    console.log(arrayElements);
 }
-
-function iAmAcallBackFunction(a, b, anyNameForCallBack){
-
-    let sum = a + b;
-    anyNameForCallBack(sum);
-
-
-
-}
-
-
-// there is alot more we can acheive throgh callback funtions but will see later InshaAllah
