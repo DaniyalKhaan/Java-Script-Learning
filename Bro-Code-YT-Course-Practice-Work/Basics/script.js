71c1e82be2e2e288fe1e0ddc904df5fedbb7a62a
Daniyal <daniyalkhan112001@gmail.com>
2024-02-25 03:28:20 +0500

    Array.forEaah()- Final

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -33,3 +33,51 @@ function printArray (arrayElements){
 anArray.forEach(function (g){
     console.log(g);
 })
+
+
+// More Examples:
+
+let namesArray = ["Danish","Rayan","Sanan","Farooq","Saifan"];
+
+namesArray.forEach(toUprCase);
+namesArray.forEach(displayElementsOfArray);
+function toUprCase (element, index, array){
+
+    array[index] = element.toUpperCase();
+
+}
+
+function displayElementsOfArray(e){
+
+    console.log(e);
+}
+
+// Next Example: ( in this example I will create array of numbers and will multiply 3 to all elements)
+
+let numbers = [1, 2, 3, 4, 5, 6];
+
+
+function multiplyByThree (element, index, array){
+
+    array[index] = element * 3;
+}
+
+numbers.forEach(multiplyByThree);
+numbers.forEach(displayElementsOfArray);
+
+// Another Example: in this example I will capatilize first letter of every element 
+// present in Array
+
+let cuteKids = ["Danish","Sanan","Farooq","Saifan","Rayan"];
+
+function capatilizeFirstLetter(element, index, array){
+
+    // array[index] = element.charAt(0).toLowerCase()+ element.slice(1);
+    array[index] = element.slice(0, 3) + element.charAt(3).toUpperCase() + element.slice(4);
+
+
+
+}
+
+cuteKids.forEach(capatilizeFirstLetter);
+cuteKids.forEach(displayElementsOfArray);
