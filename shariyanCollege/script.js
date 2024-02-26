d6fadb0124225768c02fa37889b58f8257d8d6da
Daniyal <daniyalkhan112001@gmail.com>
2024-02-26 22:27:11 +0500

    scope of var,let,and const

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,23 +1,37 @@
-// Topic: window object
+// Topic : Difference between var, let , and const
 
-// there are some/many feathures which javaScript does not have and javascript
-// access them from window object which is given by browser
+// var: js have only one variable which was var before the version jsES6
+// var have some disadvantages in which the major one was that it was
+// accesible by browser window object, it means your vaibales and arguments
+// were visible in browser 
+// due to all this var variables can change values to window variables if 
+// in case of same variables names
 
-// Example:
+// let: let was added in jsE6, it solve the issue which was with var
+// let variables is not accessible outside the program(by widow object)
+// due to this let variables does not change the default value of window
+// object variables (means we can give same name as wndow variables and it
+// will not effect original window variables)
 
-// there is alot of features which we use in our daily programming which 
-// are not part of js but a part of browser window object, we can see and 
-// check all of them in cosole if we write window there but let me mantion seome
-// here
 
-// Examples:
+// const:
 
-// window.propmt()
-// window.alert()
-// innerHTML()
-// document
-// many more
+// const was also added in jsE6, cosnt make a value constant means if 
+// someone try to change const values an error will generate
 
-// these are not the part of js instead js take these functionalities from
-// window object of browser
+// SCOPES OF JS Variables
 
+// var: var have function scope and global scope depending on where 
+// var is declared ( inside a funtion or in the top of the scroipt), 
+// var variables are hoiseted to the top of their functino of global scope
+// this means you can access them before the line of declarition but their 
+// value will be undefined
+
+// let : let are block scope means it is not accessible outside the 
+// curly brackets {} , one more difference is it is not hoisted(means
+// during runtime the declaration of let is not moved to the top)
+
+
+// const : this also have block scope, const variables needs to assign value
+// in time of declaration then afterwarards therir values will  not be changebal
+// const declarition are also not hoisted
