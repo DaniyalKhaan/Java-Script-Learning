3a5a6e4a0d3a34cd4775fc7d11956bbdfac3d050
Daniyal <daniyalkhan112001@gmail.com>
2024-02-26 21:58:49 +0500

    main-versions-of-js

---
+++ shariyanCollege/script.js
@@ -0,0 +1,13 @@
+// Topic: JacaScript versions
+
+// There are two main versions of javaScript that are used in day to day js programming
+// 1. javascript ES5, releaed in 2009
+// 2. javascript ES6, released in 2015
+
+// normally in computer languages we use only one version which is most likely the latest version but in 
+// case of js we use both the versions at a time
+
+// there are some differences in both the versions like one of the main is, the jsES5 consist of only one varible data
+// type which was var, then in jsES6 let and const was introduced, at present if we use var keyword it means we are
+// using jsES5 and if in the same program we use let or const it means we are using both the version in the program
+
