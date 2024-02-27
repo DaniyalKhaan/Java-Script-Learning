07377af9438bd4b4f1d0de3b9c3cfe05f83e08f9
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-02-27 23:43:51 +0500

    Update script.js

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,19 +1,9 @@
-// Topic : Arrays relating coping vs real coping
+// Topic: forEach
 
-// if we copy an array with assignment operator "=", the original value 
-// will share the value with the assigned array, it means both the arrays 
-// will share values so if remove or add element from one array the second array
-// will also accept changes because they are sharing the values
+// one thing to know about forEach is that it doesnt make changes to the original array
 
+let array = [12,34,54,33]
 
-// real copy:(we will use spread operator)
-// if we want to copy an array to anther array completely we will have 
-// to do this using spread operator "...", 
-// example:
-
-let array = [1,2,3];
-
-let newArray = [...array];
-
-
-console.log(newArray);
+array.forEach(function(element){
+  element * 2;
+})
