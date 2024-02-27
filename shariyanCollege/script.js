21d5ea005cec7ac6b24940602c46b333cf1fe56c
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-02-27 23:54:47 +0500

    truthy and falsy

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,9 +1,35 @@
-// Topic: forEach
+// Topic: truthy vs falsy
 
-// one thing to know about forEach is that it doesnt make changes to the original array
+// there are two types of values in js one is truthy and another one is falsy
+// these are used in conditional statements
 
-let array = [12,34,54,33]
+// falsy values: NaN, 0, undefiend, null, false and document.all
 
-array.forEach(function(element){
-  element * 2;
-})
+// truthy values: any value rather than the falsy values will be truthy
+
+// Example: if we use falsy values in conditional statements it will be considered as false
+// and if we give truthy values it will be considered as true
+
+// Truthy statements: the following statements contain truthy values and will be excecuted by the java interpreter
+if ( -2){
+}
+if ( 3){
+}
+if ( "ABCD"){
+}
+
+// falsy statements: The folowoin statements are false statements java interpreter will skip these blocks ( it will not be excecueted)
+
+if ( false ){
+}
+
+if ( 0 ){
+}
+if ( NaN ){
+}
+if ( Null ){
+}
+if ( undefined ){
+}
+if ( document.all ){
+}
