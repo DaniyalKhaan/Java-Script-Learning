07573a4aa32cfc0147d36d7794cb337744a418e6
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-02 23:27:28 +0500

    Lexical Environment

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,25 +1,6 @@
-// Topic: Heap Memory
-
-// heap memeory can also be called temporary memory in order to perfom some
-// operation we need to store some values somewhere in our computer which at
-// the point of time not are final result/answer. see the examplt below 
-// to understand this concept clearly
-
-// Example:
-
-// Imagine you have to add multiple numbers and print the sum of all the numbers
-// now, what computer does it takes one value add it with another value, then
-// store sum of both the values somewhere (heap memory) then computer add third value 
-// to the sum and so on, In this example the memory which computer needs
-// to store the temprarory result/calculation is called heap memory
-
-
-let num1 = 1;
-let num2 = 2;
-let num3 = 3;
-let num4 = 4;
-let num5 = 5;
-
-let sum = num1 + num2 + num3 + num4 + num5
-// computer will need to add the upper variable one by one and store
-// result in heap memory until the final result is not acheived
+// Topic : Lexical Environment ( Scope of a Function)
+// lexical environment is the scope of a
+// function means which variable os
+// accessible frow where and which child 
+// function can acces which properties/
+// variables etc
