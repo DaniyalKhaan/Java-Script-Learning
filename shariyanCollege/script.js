bc0d81820c4ef17b95c94d86174e560617770b36
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-02 23:35:48 +0500

    Lexical Context

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,6 +1,7 @@
-// Topic : Lexical Environment ( Scope of a Function)
-// lexical environment is the scope of a
-// function means which variable os
-// accessible frow where and which child 
-// function can acces which properties/
-// variables etc
+// Topic: Lexical Context
+
+// whenever we call a function it automatically make a imaginory container consist of three things variables, child/inside functions and lexical environment of that function . This complete method/phenomenal is call Lexical Context. 
+// 1. Variables: variable of the function which is called.
+// 2. child functions 
+// 3. lexical environment: it contain the scope of everything available inside function.
+// these three things are added automatically to an imaginary container whenever we call a function and it is call lexical context of that function.
