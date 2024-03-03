c1634dd6d5a9655d275eee16d5755db093c21faf
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-04 04:25:41 +0500

    Array.sort() METHOD

--- Bro-Code-YT-Course-Practice-Work/Basics/script.js
+++ Bro-Code-YT-Course-Practice-Work/Basics/script.js
@@ -1,14 +1,22 @@
-// Topic: array.reduce() method
+// Topic: array.sort() method
+// we can sort arrays aplphabaticaly ascending or descending , also can sort
+// numeracaly . 
 
-// we can reduce values of array to one value by using reduce array method
+// Problem to Solve: Find an smallest or largest number of an array
 
-// suppose we have an array in which prizes of all the products are stored
-// now we want to add all the prizes and from this can get the total value
-// we can acheive this using reduce method.
+// Following are the functionalites we can do by sort methods
 
-let array = [1,2,3,4,5,5,6];
-
-array.reduce(){
-    
-}
-
+/* 
+Alphabetically Ascending Sort = array.sort();
+Alphabetically desending Sort = array.reverse();
+Numerically Ascending Sort (sort() + function that return a - b);
+Numerically Descending Sort (sort() + function that return b - a);
+REMEMBER THE UPPER METHODS WILL MAKE CHANGES TO ORIGINAAL ARRAY SO IN ORDER 
+TO NOT MAKE CHANGES TO ORIGINAL ARRAY WE CAN USE THE FOLLOWING SORTED METHODS
+THAT WERE INTRODUCED IN JS ES2023:
+Array.toSorted(); This will not make changes to the original array insted will
+make a copy of and will do operations on the copy, we can also create new array
+and can assign original array to newly created array with this method so the 
+original array is safe from risk of data manipulation
+Array.toReverse; same as abobe but it will sort the new array reversily
+*/
