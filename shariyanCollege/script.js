601f3b7a42090cda1bcc2ce2de95ceaa64197f1d
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-07 05:47:10 +0500

    JS stores/converts Arrays to objects

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,7 +1,25 @@
-// Topic: Lexical Context
+// Topic: How arrays are made in behind the scences in js
+// js convert arrays to object by default
 
-// whenever we call a function it automatically make a imaginory container consist of three things variables, child/inside functions and lexical environment of that function . This complete method/phenomenal is call Lexical Context. 
-// 1. Variables: variable of the function which is called.
-// 2. child functions 
-// 3. lexical environment: it contain the scope of everything available inside function.
-// these three things are added automatically to an imaginary container whenever we call a function and it is call lexical context of that function.
+let anArray = [2,3,4,5,6];
+
+console.log(typeof(anArray));  // the output is (object), it is because the js convert the array to an object
+
+// Behind the scences js store anArray as object with index and value, see below example
+/* let anArray = {
+    0: 2,
+    1: 3,
+    2: 4,
+    3: 5,
+    4: 6
+
+} 
+*/
+
+
+
+// IF THIS IS THE CASE THEN HOW WE WILL IDENTIFY ARRAY, IF IT IS SHOWN AS OBJECT WHEN USE TYPEOF() METHOD
+// WE CAN IDENTIFY ARRAY BY isArray() method if this method return true it means it is an array if false then 
+// it is not an array
+
+console.log(Array.isArray(anArray)); // BY USING THIS METHOD WE CAN IDINTIFY ARRAY
