18e53bfc958cf46d26e17a8768ca6438f67c61c9
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-07 06:21:25 +0500

    setTimeout() Method

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,16 +1,15 @@
-// TOPIC: Passing functions as arguments to another functions
-// javascript allow to send a function as argument to anther function 
+// TOPIC: setTimeout functions
+
+// WE CAN CUSTOMIZE A FUNCTION TO EXECUTE WHEN GIVEN DELAY IS OVER
+// THE DELAY WILL BE IN milliSeconds, will put time in milliseconds of desired time duaration
 
 
 
-function functionsCanAcceptFuntionsAsArguments(anyNameForFunction){
+// SYNTAX:
+// INSIDE THE PARANTHESIS OF setTimeout(//function will be place here) a function can be made
+// and before the last paranthesis of setTimout(//, hereTimeInMilliSeconds) we will put a comma
+// then will put time duaration to stop function for
 
-    anyNameForFunction();    // you have to invoke the function you just accepted
-}
-
-
-functionsCanAcceptFuntionsAsArguments(function(){console.log("THis function was send as Argument");});
-
-
-
-
+setTimeout(function (){
+    console.log("Printed in time");
+}, 9000) 
