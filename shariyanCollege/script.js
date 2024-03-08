5c42d90df339c451baca25b50f3ca6f9c05fe25c
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-08 06:02:42 +0500

    promise syntax

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,8 +1,17 @@
-// TOPIC: JS IS SINGLE THREAD
+// Topic: promises
 
-// js is singel thread this apply even for assynchrounous code or multithreading code, JS does not execute multithreading code
-// or assynchronous concurently. Instad it execute some part of one thread fucntion and then execute some part of 
-// another thread code, then it take some more part of first thread and execute it, then agian it take some part of code
-// from second thread or any other thread. Behind the scences the execution is performed as above mentioned, but as 
-// this execution is performed at very fast speed and too small time it seems that java is executing the tasks all at a 
-// same time. BUT IN REALITY JS IS SINGLE THREAD AND IT HANDLE MULTITHREADING CODE BY TIMESHARING
+
+
+
+// SYNTAX : VARIABLE = new Promise((res,rej) => {})
+
+let x = new Promise((resolved, rejected)=>{
+    if (false){
+        console.log("This code is resolved.");
+    } else{
+        console.log("This code is rejected!");
+    }
+})
+
+
+x.then().catch();
