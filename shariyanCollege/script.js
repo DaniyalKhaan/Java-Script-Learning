dee867b6c875eff68feffeee8603821f88da6ca8
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-03-11 02:42:48 +0500

    how to apply Smooth Scrolling

--- shariyanCollege/script.js
+++ shariyanCollege/script.js
@@ -1,17 +1,22 @@
-// Topic: promises
+// Topic: Smooth Scrolling
+// to apply smooth scrolling these are the steps;
+
+//1. Search of "locomotive js github" and open the locomotive github repo/link
+
+// 2. scroll down and you will see css section in which there will be a link to css repo open that link and copy the css code
+// with out comments, paste this code in a new css file and link this file with the html file
+
+//3. now scroll below and you will see a section named "smooth", copy js code from there ( a small funcion it is)
+ // and paste it inside script.js file( this time you dont have to make a new file liks css),
+ // change the name of the element you want to apply smoth scrolling on(in most cases it will be div containg all 
+ //page such as main -etc)
+
+ // 4. now search for locomotive js cdn , open the first cdn website, there will be some option, open min js one of them
+ // and copy the link(yes address), in you html file write a script tag paste this link as "src".
 
 
+ // 5. Make sure all the files are link to the html file and is finished, smoth scrolling is applied to your document
 
-
-// SYNTAX : VARIABLE = new Promise((res,rej) => {})
-
-let x = new Promise((resolved, rejected)=>{
-    if (false){
-        console.log("This code is resolved.");
-    } else{
-        console.log("This code is rejected!");
-    }
-})
-
-
-x.then().catch();
+ // Note: when the smoth scrolling is applied to a document it may cause/make some thin lines/border at places
+ // where two div tags are intercecting. To solve this problem make the background color of the main page to black
+ // by this those lines will be dissapear.
